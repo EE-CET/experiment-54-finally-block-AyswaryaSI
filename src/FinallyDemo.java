698dce7292ebf17c7a920e5d476fda1675941969
AyswaryaSI <ayswaryasi293@gmail.com>
2026-02-13 10:34:40 +0530

    Update FinallyDemo.java

--- src/FinallyDemo.java
+++ src/FinallyDemo.java
@@ -1,19 +1,19 @@
-import java.util.Scanner;
-public class FinallyDemo {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-
-        try {
-            int result = a / b;
-            System.out.println("Result: " + result);
-        } catch (ArithmeticException e) {
-            System.out.println("Error: " + e.getMessage());
-        } finally {
-            System.out.println("Finally block executed");
-        }
-    }
+import java.util.scanner;
+public class FinallyDemo{
+       public static void main(String[]args){
+                   Scanner sc = new Scanner(System.in):
+                   int a = sc.nextInt();
+                   int b = sc.nextInt();
+                   try{
+                        int result = a/b;
+                        System.out.println("Result: " +result);
+                   }
+                   catch(ArithmeticExpression e){
+                            System.out.println("Error: "+e.getMessage());
+                   }
+                   finally{
+                            System.out.println("Finally block executed");
+                   }
+       }
 }
 
